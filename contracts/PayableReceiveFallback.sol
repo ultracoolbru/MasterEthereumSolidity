5476b1cad7cd36c809a78069758f58d197c37b22
Remix Workspace <>
2022-11-03 18:05:18 +0300

    -

--- contracts/PayableReceiveFallback.sol
+++ contracts/PayableReceiveFallback.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.17;
+pragma solidity >=0.8.17;
 
-contract Deposit {
+contract PayableReceiveFallback {
     receive() external payable {}
 
     fallback() external payable {}
 
     function getBalance() public view returns(uint) {
         return address(this).balance;
+    }
+
+    function send() public payable {
+        uint i;
+        i++;
     }
 }
